9f8cdfddf55d0c04e3bd53436515c88ed1255cd8
ikenatsu <kei.trmc.s2-2sc@outlook.jp>
2023-11-19 01:03:54 +0900

    手数料及び利益の非同期通信

---
+++ app/javascript/item_price.js
@@ -0,0 +1,10 @@
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  const addTaxDom = document.getElementById("add-tax-price")
+  addTaxDom.innerHTML = Math.floor( inputValue/10 )
+  console.log(addTaxDom.innerHTML)
+  const addprofit = document.getElementById("profit")
+  addprofit.innerHTML = Math.floor( inputValue - addTaxDom.innerHTML )
+  console.log(addprofit.innerHTML)
+})
