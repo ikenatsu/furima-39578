1a56e16bb776bcbf6cdb20982a0c39b90f6b5320
ikenatsu <kei.trmc.s2-2sc@outlook.jp>
2023-11-26 14:09:02 +0900

    コントローラ内の同じ記述をメソッド化して集約

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   # before_action :move_to_index, except: [:index, :show]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,18 +21,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless current_user == @item.user
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to item_path(@item)
@@ -52,5 +50,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :comment, :category_id, :condition_id, :shipping_cost_id, :prefecture_id,
                                  :shipping_duration_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
